0a9983c89cc160410310d8dcba82100b3732811b
RODRIGAMER669 <alexis.rr0501@gmail.com>
2025-05-16 10:27:33 +0200

    Cambios en hola mundo

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,2 +1,16 @@
 # Mi primer Hola Mundo!
 print("Hola mundo!")
+print('Hola mundo!')
+
+
+
+"""
+Este es un comentario 
+para varias
+lineas
+"""
+'''
+Tambien es un comentario
+en varias 
+lineas
+'''

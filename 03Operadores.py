158ff711ea3297c196e43bf6efcd1d0aa4694038
RODRIGAMER669 <alexis.rr0501@gmail.com>
2025-05-19 10:23:15 +0200

    Operadores

---
+++ 03Operadores.py
@@ -0,0 +1,43 @@
+# Operadores Aritmeticos
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3) # Aproxima
+print(2 ** 3)
+
+print("Hola " + "python " + "patata")
+print("Hola " + str(5))
+print("Hola " * 5)
+
+my_float = 2.5 * 2
+print("hola " * int(my_float))
+
+
+# Operadores comparativos
+
+print( 3 > 4)
+print( 3 < 4)
+print( 3 >= 4)
+print( 3 <= 4)
+print( 3 == 4)
+print( 3 != 4)
+
+print( "Hola" > "python")
+print( "Hola" <= "python")
+print( "Hola" >= "python")
+print( "Hola" < "python")
+print( "Hola" == "python")
+print( "Hola" != "python")
+
+print( "Hola" >= "Zola") # Ordenación alfabética por caracteres (min y mayus tambien)
+print( "aaa" >= "aba")
+print( len("aaa") >= len("aaa"))
+
+
+# Operadores lógicos
+print( 3 > 4 and "Hola" > "python")
+print( 3 > 4 or "Hola" > "python")
+print( not(3 > 4))
